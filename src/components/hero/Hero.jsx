2c8f2bf3b865bb0fa7166b6ad0b1fafe60a46add
rayhanm17 <marrero.rayhan@gmail.com>
2023-11-14 18:07:40 -0500

    sin wave update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,43 +5,44 @@ import { Points, PointMaterial } from '@react-three/drei';
 import Button from '../button/Button';
 
 import Styles from './hero.module.sass'
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import { AmbientLightProbe } from 'three';
 
-const WaveDots = (props) => {
+
+const WaveDots = ({numPoints, frequency, amplitude}) => {
+  const {height, width} = useWindowDimensions()
+
   const ref = useRef()
-
   
-  const amplitude = 1.0;      // Amplitude of the sine wave
-  const frequency = 1.0;      // Frequency of the sine wave (number of cycles within 2*pi)
-  const numPoints = 300;    // Number of points to generate
-  
-  const sineWavePoints = new Float32Array(numPoints * 2);
+  const wavePoints = new Float32Array(numPoints * 2);
   
   for (let i = 0; i < numPoints; i++) {
-    const x = (i / (numPoints - 1)) * 4 * Math.PI - 2 * Math.PI; // X-coordinate within the -2π to 2π range
-    const y = amplitude * Math.sin(frequency * x);  // Calculate y-coordinate using the sine function
-    sineWavePoints[i * 2] = x;
-    sineWavePoints[i * 2 + 1] = y;
+    const x = width / 800 * Math.sin(i); // domain of the function
+    const y = amplitude * Math.sin(frequency * i);  // Calculate y-coordinate using the sine function
+
+    wavePoints[i*2] = x;
+    wavePoints[i*2 + 1] = y;
   }
 
-
-
   useFrame((state, delta) => {
+
     // Update the positions of the points based on time (delta)
     for (let i = 0; i < numPoints; i++) {
       // Update the y-coordinate of each point based on a sine function and time
-      const x = sineWavePoints[i * 2];
+      const x = wavePoints[i*2]
       const time = state.clock.getElapsedTime(); // Get elapsed time
-      sineWavePoints[i * 2 + 1] = amplitude * Math.sin(frequency * x + time);
+      wavePoints[i*2 + 1] = amplitude * Math.sin(frequency * x + time);
+
     }
   
   });
 
   return (
     <group>
-      <Points ref={ref} positions={sineWavePoints} stride={2} frustumCulled {...props}>
+      <Points ref={ref} positions={wavePoints} stride={2} frustumCulled>
         <PointMaterial
           color="#114B5F"
-          size={0.02}
+          size={0.01}
           sizeAttentuation={true}
           depthWrite={false}
         />
@@ -51,11 +52,17 @@ const WaveDots = (props) => {
 };
 
 const Hero = () => {
+  const {height, width} = useWindowDimensions()
+
+  const amplitude = 1.0;      // Amplitude of the sine wave
+  const frequency = 4.0;     // Frequency of the sine wave (number of cycles within 2*pi)
+  const numPoints = 1000;   // Number of points to generate
+
   return (
     <div className={Styles.fullscreen}>
-      <Canvas camera={{ position: [0, 0, 5] }}>
+      <Canvas camera={{position: [0, 0, amplitude*1.35], fov: 75}}>
         <Suspense fallback={null}>
-          <WaveDots />
+          <WaveDots amplitude={amplitude} frequency={frequency} numPoints={numPoints}/>
         </Suspense>
       </Canvas>
       <div className={Styles.absolutecenter}>
